d5ef7ef5a798be6a12023abbcd9d7d8cd789b7cc
INPGmariuszK <kuchtamariusz.inpg2019@gmail.com>
2019-06-01 10:50:05 +0200

    Implementacja metody main_structure (stowrzenie układu przycisków)

--- GUI.py
+++ GUI.py
@@ -32,7 +32,48 @@ class WindowWithButtons(QWidget):
         self.main_structure()
 
     def main_structure(self):
-        pass
+
+        self.setGeometry(500, 500, 500, 500)
+        self.setWindowTitle('Kółko i krzyżyk')
+
+        # Do czasu znalezienia ikon, linijka nieaktywna
+        # self.setWindowIcon(QIcon("graphics/krzyz.ico"))
+
+        for button_nmb in range(0, 9):
+            self.buttons.append(QPushButton(EMPTY, self))
+            self.buttons[button_nmb].clicked.connect(self.button_clicked)
+            self.buttons[button_nmb].setObjectName("button%d" % button_nmb)
+            self.buttons[button_nmb].resize(100, 100)
+
+            # Do czasu znalezienia ikon, linijki nieaktywne
+            # self.buttons[button_nmb].setIcon(QIcon("EMPTY_ICON.ico"))
+            # self.buttons[button_nmb].setIconSize(QtCore.QSize(50, 50))
+
+        self.buttons[0].move(50, 50)
+
+        self.buttons[1].move(200, 50)
+
+        self.buttons[2].move(350, 50)
+
+        self.buttons[3].move(50, 200)
+
+        self.buttons[4].move(200, 200)
+
+        self.buttons[5].move(350, 200)
+
+        self.buttons[6].move(50, 350)
+
+        self.buttons[7].move(200, 350)
+
+        self.buttons[8].move(350, 350)
+
+        self.show()
+
+
+        # Do czasu znalezienia dzwięków, linijki nieaktywne
+        # mixer.music.load('music/background_music.wav')
+        # mixer.music.play(-1)
+
 
     def button_clicked(self):
         pass
